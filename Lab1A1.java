57f74d531a102d472b7fbbfbfc56d4b4caab8743
Parva_Karia_28 <143929422+parva8@users.noreply.github.com>
2024-03-26 12:29:30 +0530

    Add files via upload

---
+++ Lab1A1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Lab1A1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        double a = sc.nextDouble();
+        System.out.println("enter number");
+        double b = sc.nextDouble();
+        double c = a + b;
+        System.out.println("the sum is "+c);
+    }
+}
